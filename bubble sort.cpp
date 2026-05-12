b662c07db59c34526de6a1814b9981644d19d639
Remy-fullstack-developer <nsabimanajeanremy39@gmail.com>
2026-05-12 14:20:05 +0200

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int roll_numbers[4]={98,43,65,43};
+	
+	cout<<"array before sorting"<<endl;
+	
+	
+	
+	for(int i=0; i<4; i++){
+		   	cout<<"array after sorting"<<endl;
+			cout<<roll_numbers[i]<<endl;
+		
+		for(int j=0; j<4; i++){
+			
+			if(roll_numbers[j]>roll_numbers[j+1]){
+				
+				
+				int temp=roll_numbers[j];
+				roll_numbers[j]
+			}
+		}
+		
+	
+	}
+	
+	
+	return 0;
+}
